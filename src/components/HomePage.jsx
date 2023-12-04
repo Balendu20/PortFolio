88c287f40bcc2e15d2aa5ed5f505d5e58a8f29d7
Balendu20 <msbalu2424250@gmail.com>
2023-12-04 23:25:20 +0530

    display update

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = ({ home }) => {
     <div ref={home} className="p1">
       <img src="sec1.jpg" className="sec1"></img>
       <img src="pic.png" className="mypic"></img>
-      <div className=".parent-container">
+      <div className="">
         <container className="text-label">
           <h2>Hi Everyone, I am</h2>
           <h1>Balendu M S</h1>
